98ea93598ae85788b062f9b98b21ee88b66fa399
LeoZhang553 <leoz553@deltalearns.ca>
2020-09-29 12:22:58 -0700

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -19,14 +19,37 @@ L3=input('enter line 3: ')
 L1=float(L1)
 L2=float(L2)
 L3=float(L3)
-a=L2**2
-b=L3**2
-c=L1**2
+if L1 < L3 and L2 < L3:
+    a=L1**2
+    b=L2**2
+    c=L3**2
+elif L1 > L2 and L1 > L3:
+    c=L1**2
+    b=L2**2
+    a=L3**2
+elif L2 > L1 and L2 > L3:
+    a=L1**2
+    c=L2**2
+    b=L3**2
+elif L1 < L2 and L1 < L3:
+    a=L3**2
+    b=L2**2
+    c=L1**2
+elif L2 < L1 and L2 < L3:
+    a=L3**2
+    b=L1**2
+    c=L2**2
+elif L3 < L2 and L3 < L1:
+    a=L3**2
+    b=L1**2
+    c=L3**2
+
 if a+b >= c*0.98 and a+b <= c*1.02:
     print('that is a right triangle')
 elif a+b > c*1.02:
     print('that is an acute triangle')
 elif a+b < c*0.98:
     print('that is an obtuse triangle')
+
     
     
