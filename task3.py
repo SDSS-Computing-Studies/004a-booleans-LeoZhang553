b87a30fe6bed9fab9ee1d856756326ed34428cf7
LeoZhang553 <leoz553@deltalearns.ca>
2020-09-29 11:38:43 -0700

    update

--- task3.py
+++ task3.py
@@ -21,10 +21,10 @@
 N=input('number=')
 N=float(N)
 if N >= 1000:
-    print("The number is equal to 1000 or is larger than 1000")
+    print("3")
 elif N >= 100:
-    print('The number is 100 or a number up to 1000')
+    print('2')
 elif N >= 10:
-    print('The number is 10 or a number up to 100')
+    print('1')
 else:
-    print('The number is 0 or a number up to 10')
+    print('0')
